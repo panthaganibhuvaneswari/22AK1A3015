2d106c9bacfe04e1580d7a2a3dfc676aa01617bf
sneha <alisettysneha@gmail.com>
2024-09-14 12:30:50 +0530

    Create substring 0f even.py

---
+++ substring 0f even.py
@@ -0,0 +1,13 @@
+def even_level_substring(input_string):
+    # Extract characters at even indices using slicing
+    even_level_string = input_string[::2]
+    return even_level_string
+
+# Input string
+input_string = "Sunrises in the east"
+
+# Call the function to get the substring with even level characters
+result = even_level_substring(input_string)
+
+# Output the result
+print("The substring with characters at even levels is:", result)
